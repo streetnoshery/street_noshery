173e8e3497941ef7b6d15df5fc426dae2e570299
streetnoshery <streetnoshery@gmail.com>
2025-03-15 19:44:52 +0530

    SN-1, app review API

--- lib/reviews/controller/street_noshery_app_review_controller.dart
+++ lib/reviews/controller/street_noshery_app_review_controller.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:street_noshery/common/common_response.dart';
 import 'package:street_noshery/firebase/firebase_model/street_noshery_review_static_data.model.dart';
+import 'package:street_noshery/home_page/controllers/home_controller.dart';
 import 'package:street_noshery/onboarding/controllers/street_noshery_onboarding_controller.dart';
 import 'package:street_noshery/reviews/model/street_noshery_review_model.dart';
 import 'package:street_noshery/reviews/provider/street_noshery_review_providers.dart';
@@ -14,9 +15,17 @@ class StreetnosheryAppReviewController extends GetxController {
   final onboardingController = Get.find<StreetNosheryOnboardingController>();
   StreetNosheryReviewFirebaseModel get streetNosheryReviewFirebasemodel => onboardingController.fireBaseContentHandler.streetNosheryReviewFirebaseModel;
   StreetNosheryReviewmodel reviewmodel = StreetNosheryReviewmodel();
+  final homeController = Get.find<StreetNosheryHomeController>();
 
   void increaseStarCount(int index) {
     selectedStars.value = index + 1;
+  }
+
+  @override
+  void onInit() async {
+    await homeController.reviews();
+    selectedStars.value = homeController.ratings.averageRating?.toInt() ?? 0;
+    super.onInit();
   }
 
   Future<void> updateReview(num? rating, String? reviews) async {
