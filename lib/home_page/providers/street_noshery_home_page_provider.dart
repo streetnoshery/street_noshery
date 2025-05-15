28155baac18aaa606577787a7280238611d798a0
streetnoshery <streetnoshery@gmail.com>
2025-05-15 12:28:30 +0530

    SN-removed mock responses from providers

--- lib/home_page/providers/street_noshery_home_page_provider.dart
+++ lib/home_page/providers/street_noshery_home_page_provider.dart
@@ -26,47 +26,17 @@ class StreetNosheryHomeProviders {
   Future<RepoResponse> getMenu({int? shopId}) async {
     try {
       final String finalUrl = commonHost.url(StreetNosheryUrls.getMenu);
-      final response = {
-        "state": "SUCCESS",
-        "status": 200,
-        "data": {
-          "_id": "67b1d6d6aae1dc9d6cb11226",
-          "shopId": 1,
-          "__v": 0,
-          "createdAt": "2025-02-16T12:15:18.243Z",
-          "menu": [
-            {
-              "dishName": "Chole kulche",
-              "description": "Creamy tomato-based curry with paneer",
-              "price": "250",
-              "rating": 5,
-              "foodId": 170263,
-              "ratingCount": 1,
-              "image": "assets/home/street_noshery_dark_green_logo.png",
-              "category": "food"
-            },
-            {
-              "dishName": "Paneer Masala",
-              "description": "Creamy tomato-based curry with paneer",
-              "price": "350",
-              "rating": 5,
-              "foodId": 639167,
-              "ratingCount": 1,
-              "category": "breakfast",
-              "image": "assets/home/street_noshery_dark_green_logo.png"
-            }
-          ],
-          "updatedAt": "2025-04-07T14:38:50.833Z"
-        }
-      };
-      // await api.request(
-      //     apiString: finalUrl,
-      //     method: "get",
-      //     queryParams: {"shopId": shopId.toString()});
+      final response = await api.request(
+          apiString: finalUrl,
+          method: "get",
+          queryParams: {"shopId": shopId.toString()});
 
-      return RepoResponse<StreetNosheryMenu>(
-          data: StreetNosheryMenu.fromJson(
-              response == '' ? {} : response["data"] as Map<String, dynamic>));
+      return response is ApiException
+          ? RepoResponse<ApiException>(error: response, data: null)
+          : RepoResponse<StreetNosheryMenu>(
+              data: StreetNosheryMenu.fromJson(response == ''
+                  ? {}
+                  : response["data"] as Map<String, dynamic>));
     } catch (e) {
       rethrow;
     }
@@ -75,24 +45,16 @@ class StreetNosheryHomeProviders {
   Future<RepoResponse> getReviews({int? shopId}) async {
     try {
       final String finalUrl = commonHost.url(StreetNosheryUrls.getReview);
-      final response = {
-        "state": "SUCCESS",
-        "status": 200,
-        "data": {
-          "shopId": 1,
-          "totalRating": 10,
-          "ratingCount": 2,
-          "averageRating": 5
-        }
-      };
-      // await api.request(
-      //     apiString: finalUrl,
-      //     method: "get",
-      //     queryParams: {"shopId": shopId.toString()});
+      final response = await api.request(
+          apiString: finalUrl,
+          method: "get",
+          queryParams: {"shopId": shopId.toString()});
 
-      return RepoResponse<StreetNosheryShopRating>(
-          data: StreetNosheryShopRatingMapper.fromJson(
-              response == '' ? "" : jsonEncode(response["data"])));
+      return response is ApiException
+          ? RepoResponse<ApiException>(error: response, data: null)
+          : RepoResponse<StreetNosheryShopRating>(
+              data: StreetNosheryShopRatingMapper.fromJson(
+                  response == '' ? "" : jsonEncode(response["data"])));
     } catch (e) {
       rethrow;
     }
@@ -121,204 +83,16 @@ class StreetNosheryHomeProviders {
   Future<RepoResponse> getPastOrders({String? customerId}) async {
     try {
       final String finalUrl = commonHost.url(StreetNosheryUrls.getPastOrders);
-      final response = {
-        "state": "SUCCESS",
-        "status": 200,
-        "data": [
-          {
-            "_id": "67f6cb689ce2a84ed79c0d6e",
-            "orderTrackId": "vXI3OZw53DZyggN1",
-            "__v": 0,
-            "createdAt": "2025-04-09T19:32:56.601Z",
-            "customerId": "STREET_NOSHERY_Q9TQEDQMJZ80H85HANQN",
-            "isOrderConfirmed": true,
-            "isOrderDelivered": true,
-            "isOrderFailed": false,
-            "isOrderInProgress": false,
-            "isOrderOutForDelivery": true,
-            "isOrderPlaced": true,
-            "isPaymentDone": true,
-            "isorderCancelled": false,
-            "orderItems": [
-              {
-                "dishName": "Chole kulche",
-                "description":
-                    "A rich and creamy tomato-based curry with paneer.",
-                "price": "250",
-                "rating": 4,
-                "foodId": 170263,
-                "count": 1
-              },
-              {
-                "dishName": "Garlic Naan",
-                "description":
-                    "Soft Indian bread topped with garlic and butter.",
-                "price": "50",
-                "rating": 5,
-                "foodId": 639167,
-                "count": 2
-              }
-            ],
-            "orderPlacedAt": "2025-04-09T19:32:56.600Z",
-            "orderStatus": "DELIVERED",
-            "paymentAmount": "350",
-            "paymentId": "RRCNJIRUNSC_KJDCNJD",
-            "paymentStatus": "SUCCESS",
-            "razorpayOrderId": "RRCNJIRUNSC_KJDCNJD",
-            "shopId": 1,
-            "updatedAt": "2025-04-09T19:39:49.738Z",
-            "orderConfirmedAt": "2025-04-09T19:34:00.824Z",
-            "orderOutForDeliveryAt": "2025-04-09T19:38:41.928Z",
-            "orderDeliveredAt": "2025-04-09T19:39:49.736Z"
-          },
-          {
-            "_id": "67f6cb689ce2a84ed79c0d6e",
-            "orderTrackId": "vXI3OZw53DZyggN2",
-            "__v": 0,
-            "createdAt": "2025-04-09T19:32:56.601Z",
-            "customerId": "STREET_NOSHERY_Q9TQEDQMJZ80H85HANQN",
-            "isOrderConfirmed": true,
-            "isOrderDelivered": true,
-            "isOrderFailed": false,
-            "isOrderInProgress": false,
-            "isOrderOutForDelivery": true,
-            "isOrderPlaced": true,
-            "isPaymentDone": true,
-            "isorderCancelled": false,
-            "orderItems": [
-              {
-                "dishName": "Chole kulche",
-                "description":
-                    "A rich and creamy tomato-based curry with paneer.",
-                "price": "250",
-                "rating": 4,
-                "foodId": 170263,
-                "count": 1
-              },
-              {
-                "dishName": "Garlic Naan",
-                "description":
-                    "Soft Indian bread topped with garlic and butter.",
-                "price": "50",
-                "rating": 5,
-                "foodId": 639167,
-                "count": 2
-              }
-            ],
-            "orderPlacedAt": "2025-04-09T19:32:56.600Z",
-            "orderStatus": "DELIVERED",
-            "paymentAmount": "350",
-            "paymentId": "RRCNJIRUNSC_KJDCNJD",
-            "paymentStatus": "SUCCESS",
-            "razorpayOrderId": "RRCNJIRUNSC_KJDCNJD",
-            "shopId": 1,
-            "updatedAt": "2025-04-09T19:39:49.738Z",
-            "orderConfirmedAt": "2025-04-09T19:34:00.824Z",
-            "orderOutForDeliveryAt": "2025-04-09T19:38:41.928Z",
-            "orderDeliveredAt": "2025-04-09T19:39:49.736Z"
-          },
-          {
-            "_id": "67f6cb689ce2a84ed79c0d6e",
-            "orderTrackId": "vXI3OZw53DZyggN3",
-            "__v": 0,
-            "createdAt": "2025-04-09T19:32:56.601Z",
-            "customerId": "STREET_NOSHERY_Q9TQEDQMJZ80H85HANQN",
-            "isOrderConfirmed": true,
-            "isOrderDelivered": true,
-            "isOrderFailed": false,
-            "isOrderInProgress": false,
-            "isOrderOutForDelivery": true,
-            "isOrderPlaced": true,
-            "isPaymentDone": true,
-            "isorderCancelled": false,
-            "orderItems": [
-              {
-                "dishName": "Chole kulche",
-                "description":
-                    "A rich and creamy tomato-based curry with paneer.",
-                "price": "250",
-                "rating": 4,
-                "foodId": 170263,
-                "count": 1
-              },
-              {
-                "dishName": "Garlic Naan",
-                "description":
-                    "Soft Indian bread topped with garlic and butter.",
-                "price": "50",
-                "rating": 5,
-                "foodId": 639167,
-                "count": 2
-              }
-            ],
-            "orderPlacedAt": "2025-04-09T19:32:56.600Z",
-            "orderStatus": "DELIVERED",
-            "paymentAmount": "350",
-            "paymentId": "RRCNJIRUNSC_KJDCNJD",
-            "paymentStatus": "SUCCESS",
-            "razorpayOrderId": "RRCNJIRUNSC_KJDCNJD",
-            "shopId": 1,
-            "updatedAt": "2025-04-09T19:39:49.738Z",
-            "orderConfirmedAt": "2025-04-09T19:34:00.824Z",
-            "orderOutForDeliveryAt": "2025-04-09T19:38:41.928Z",
-            "orderDeliveredAt": "2025-04-09T19:39:49.736Z"
-          },
-          {
-            "_id": "67f6cb689ce2a84ed79c0d6e",
-            "orderTrackId": "vXI3OZw53DZyggN4",
-            "__v": 0,
-            "createdAt": "2025-04-09T19:32:56.601Z",
-            "customerId": "STREET_NOSHERY_Q9TQEDQMJZ80H85HANQN",
-            "isOrderConfirmed": true,
-            "isOrderDelivered": true,
-            "isOrderFailed": false,
-            "isOrderInProgress": false,
-            "isOrderOutForDelivery": true,
-            "isOrderPlaced": true,
-            "isPaymentDone": true,
-            "isorderCancelled": false,
-            "orderItems": [
-              {
-                "dishName": "Chole kulche",
-                "description":
-                    "A rich and creamy tomato-based curry with paneer.",
-                "price": "250",
-                "rating": 4,
-                "foodId": 170263,
-                "count": 1
-              },
-              {
-                "dishName": "Garlic Naan",
-                "description":
-                    "Soft Indian bread topped with garlic and butter.",
-                "price": "50",
-                "rating": 5,
-                "foodId": 639167,
-                "count": 2
-              }
-            ],
-            "orderPlacedAt": "2025-04-09T19:32:56.600Z",
-            "orderStatus": "DELIVERED",
-            "paymentAmount": "350",
-            "paymentId": "RRCNJIRUNSC_KJDCNJD",
-            "paymentStatus": "SUCCESS",
-            "razorpayOrderId": "RRCNJIRUNSC_KJDCNJD",
-            "shopId": 1,
-            "updatedAt": "2025-04-09T19:39:49.738Z",
-            "orderConfirmedAt": "2025-04-09T19:34:00.824Z",
-            "orderOutForDeliveryAt": "2025-04-09T19:38:41.928Z",
-            "orderDeliveredAt": "2025-04-09T19:39:49.736Z"
-          }
-        ]
-      };
-      // await api.request(
-      //     apiString: finalUrl,
-      //     method: "get",
-      //     queryParams: {"customerId": customerId});
+      final response = await api.request(
+          apiString: finalUrl,
+          method: "get",
+          queryParams: {"customerId": customerId});
 
-      return RepoResponse<StreetNosheryPastorderDataResponseModel>(
-          data: StreetNosheryPastorderDataResponseModel.fromJson(
-              response == '' ? {} : response));
+      return response is ApiException
+          ? RepoResponse<ApiException>(error: response, data: null)
+          : RepoResponse<StreetNosheryPastorderDataResponseModel>(
+              data: StreetNosheryPastorderDataResponseModel.fromJson(
+                  response == '' ? {} : response));
     } catch (e) {
       rethrow;
     }
--- lib/onboarding/providers/street_noshery_onboarding_providers.dart
+++ lib/onboarding/providers/street_noshery_onboarding_providers.dart
@@ -27,16 +27,18 @@ class StreetNosheryOnboardingProviders {
       {String? mobileNumber, StreetNosheryOnboardingEnums? objective}) async {
     try {
       final String finalUrl = commonHost.url(StreetNosheryUrls.generateOtp);
-      final response = {"state": "SUCCESS", "status": 201, "data": "ok"};
-      //     await api.request(apiString: finalUrl, method: "post", payload: {
-      // "mobileNumber": mobileNumber,
-      // "reason":
-      // objective?.toString().split('.').last // Convert enum to string
-      // });
+      final response =
+          await api.request(apiString: finalUrl, method: "post", payload: {
+        "mobileNumber": mobileNumber,
+        "reason":
+            objective?.toString().split('.').last // Convert enum to string
+      });
 
-      return RepoResponse<StreetNosheryOtpResponse>(
-          data: StreetNosheryOtpResponse.fromJson(
-              response == '' ? {} : response));
+      return response is ApiException
+          ? RepoResponse<ApiException>(error: response, data: null)
+          : RepoResponse<StreetNosheryOtpResponse>(
+              data: StreetNosheryOtpResponse.fromJson(
+                  response == '' ? {} : response));
     } catch (e) {
       rethrow;
     }
@@ -48,17 +50,19 @@ class StreetNosheryOnboardingProviders {
       String? otp}) async {
     try {
       final String finalUrl = commonHost.url(StreetNosheryUrls.verifyOtp);
-      final response = {"state": "SUCCESS", "status": 201, "data": "ok"};
-      //     await api.request(apiString: finalUrl, method: "post", payload: {
-      // "mobileNumber": mobileNumber,
-      // "reason":
-      // objective?.toString().split('.').last, // Convert enum to string,
-      // "otp": otp
-      // });
+      final response =
+          await api.request(apiString: finalUrl, method: "post", payload: {
+        "mobileNumber": mobileNumber,
+        "reason":
+            objective?.toString().split('.').last, // Convert enum to string,
+        "otp": otp
+      });
 
-      return RepoResponse<StreetNosheryOtpResponse>(
-          data: StreetNosheryOtpResponse.fromJson(
-              response == '' ? {} : response));
+      return response is ApiException
+          ? RepoResponse<ApiException>(error: response, data: null)
+          : RepoResponse<StreetNosheryOtpResponse>(
+              data: StreetNosheryOtpResponse.fromJson(
+                  response == '' ? {} : response));
     } catch (e) {
       rethrow;
     }
@@ -67,38 +71,17 @@ class StreetNosheryOnboardingProviders {
   Future<RepoResponse> getUser(String mobileNumber) async {
     try {
       final String finalUrl = commonHost.url(StreetNosheryUrls.getUser);
-      var response = {
-        "state": "SUCCESS",
-        "status": 200,
-        "data": {
-          "_id": "680382969ce2a84ed79c5f60",
-          "mobileNumber": "8107748619",
-          "__v": 0,
-          "countryCode": "+91",
-          "createdAt": "2025-04-19T11:01:42.100Z",
-          "customerId": "STREET_NOSHERY_TF6W36LQW9BPDOY5J6ZS",
-          "isEmailNotificationEnable": true,
-          "isRegisterForShop": false,
-          "status": "USER_DETAILS_VERIFICATION",
-          "updatedAt": "2025-04-20T11:42:55.792Z",
-          "email": "sumitgod510@gmail.com",
-          "password": "Sumit@1062",
-          "address": {
-            "firstLine": "Sigma Tech Park",
-            "secondLine": "Bangalore",
-            "shopId": 1
-          },
-          "userName": "Sumit Kumar Godwan"
-        }
-      };
-      // await api.request(
-      // apiString: finalUrl,
-      // method: "get",
-      // queryParams: {"mobileNumber": mobileNumber});
+      var response = await api.request(
+          apiString: finalUrl,
+          method: "get",
+          queryParams: {"mobileNumber": mobileNumber});
 
-      return RepoResponse<StreetNosheryUser>(
-          data: StreetNosheryUser.fromJson(
-              response == '' ? {} : response["data"] as Map<String, dynamic>));
+      return response is ApiException
+          ? RepoResponse<ApiException>(error: response, data: null)
+          : RepoResponse<StreetNosheryUser>(
+              data: StreetNosheryUser.fromJson(response == ''
+                  ? {}
+                  : response["data"] as Map<String, dynamic>));
     } catch (e) {
       rethrow;
     }
